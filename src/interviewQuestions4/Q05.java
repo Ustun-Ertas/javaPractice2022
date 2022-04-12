9af9f225b1b013b2a0cec70ae0b707ccf8bb646f
Ustun Ertas <ustunertas0@gmail.com>
2022-04-12 23:02:54 +0200

    pratice

--- src/interviewQuestions4/Q05.java
+++ src/interviewQuestions4/Q05.java
@@ -1,5 +1,8 @@
 package interviewQuestions4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Q05 {
 
             /*
@@ -19,7 +22,24 @@ public class Q05 {
             Cevap = -1 olmalı.
       */
 
-
-
-
+    public static void main(String[] args) {
+        // ArrayList<String> arrlist = new ArrayList<>(Arrays.asList("$13", "$15", "$20"));
+        ArrayList<String> arrlist = new ArrayList<>(Arrays.asList("$13", "$15", "$-30"));
+        System.out.println("toplam : " + getSum(arrlist));
+    }
+    private static int getSum(ArrayList<String> arrlist) {
+        int toplam = 0;
+        for (String  each : arrlist) {
+            String str = each.replace("$", "");
+            toplam += Integer.parseInt(str); //string olan sayi degerlerini integer a cevirdik ve toplama ekledik
+        }
+        if(toplam<0){
+            return -1; // toplam 0 dan kucuk oldugu icin method -1 dondursun dedigi icin yaptik..
+        }else
+            return toplam;
+    }
 }
+
+
+
+
