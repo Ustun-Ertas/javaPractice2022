6f26285e2a6b00793a006f87f590614058142767
Ustun Ertas <ustunertas0@gmail.com>
2022-04-12 22:48:55 +0200

    pratice

---
+++ src/practice_free/Arrays.java
@@ -0,0 +1,28 @@
+package practice_free;
+
+public class Arrays {
+
+  /*
+        Bir siniftaki ogrencilerin boylari tamsayi olarak sirasiz veriliyor,
+        ayrica bu sinifin ogrencisi olan Ali'nin boyu veriliyor. Beden egitimi dersinde
+        buyukten kucuge dogru ogrenciler siralandiginda Ali kacinci sirada olur(en uzun
+        ogrenci 1. siradadir, Ali'nin boyu verilen dizide de yer almaktadir
+
+         */
+
+    //Once Ali'den kac kisi uzun oldugunu bulmam gerekiyor
+    //Sonra Ali ile ayni boyda kac kisi var onu bulmam lazim
+    //Ve son olarakta Ali'nin muhtemel siralari
+
+    public static void main(String[] args) {
+
+        int[] boylar = new int[]{190, 170, 185, 170, 177, 178, 198, 170, 187, 182};
+
+
+
+
+
+
+    }
+
+}
