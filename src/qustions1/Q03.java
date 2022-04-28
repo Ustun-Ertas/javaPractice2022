c56784a1b94461786e4542e5f7a0c45788b21750
Ustun Ertas <ustunertas0@gmail.com>
2022-04-28 22:58:03 +0200

    28.04.22

--- src/qustions1/Q03.java
+++ src/qustions1/Q03.java
@@ -9,26 +9,32 @@ public class Q03 {
                 */
 
     public static void main(String[] args) {
-        Scanner scanner =new Scanner(System.in);
-        System.out.print("Bir sayi giriniz :");
-        int sayi= scanner.nextInt();
-
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Bir sayi giriniz :");
+        int sayi=scan.nextInt();
 
         boolean asalMi=true;
 
         if (sayi>=2){
-            for (int i = 2; i <sayi ; i++) {
 
+            for (int i = 2; i <sayi ; i++) {
                 if (sayi%i==0){
                     asalMi=false;
                     break;
+
                 }
 
             }
-            if(asalMi) System.out.println("sayi asaldir");
-            else System.out.println("sayi asal degildir");
-        }
+            if (asalMi){
+                System.out.println("sayi asaldir");
 
+            }else {
+                System.out.println("sayi asal degildir");
+            }
+
+        } else {
+            System.out.println("sayi asal degildir");//bu else yi 0 ve 1 in asal sayi olmadigini gostermek icin ekledim
+        }
 
     }
 }
