eb5a45d95e0789fae0df0d8ba679397efd4d0d53
Ustun Ertas <ustunertas0@gmail.com>
2023-03-16 22:02:22 +0300

    project

--- src/practice_basic_day03/Q13_WhileLoop01.java
+++ src/practice_basic_day03/Q13_WhileLoop01.java
@@ -3,4 +3,18 @@ package practice_basic_day03;
 public class Q13_WhileLoop01 {
     // girilen sayının basamaklarındaki rakamların toplamını bulunuz.
 
+    public static void main(String[] args) {
+
+        int sayi = 1234;
+        int toplam = 0;
+
+        while(sayi > 0) {
+            toplam += sayi % 10;
+            sayi /= 10;
+        }
+
+        System.out.println("Basamaklarındaki rakamların toplamı: " + toplam);
+
+    }
+
 }
--- src/practice_basic_day03/Q14_WhileLoop02.java
+++ src/practice_basic_day03/Q14_WhileLoop02.java
@@ -1,9 +1,24 @@
 package practice_basic_day03;
 
+import java.util.Scanner;
+
 public class Q14_WhileLoop02 {
     /*
          Girilen bir sayıya kadar olan sayılardan sadece tek olanlarını ekrana yazdırınız.
          girilen sayı dahil
         */
 
-}
+    public static void main(String[] args) {
+
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Bir sayı giriniz: ");
+                int sayi = scanner.nextInt();
+
+                System.out.println("Girilen sayı dahil, tek sayılar:");
+                for (int i = 1; i <= sayi; i += 2) {
+                    System.out.print(i + " ");
+                }
+            }
+        }
+
+
