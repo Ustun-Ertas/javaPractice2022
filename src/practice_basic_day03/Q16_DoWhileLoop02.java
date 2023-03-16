ceb7c1eff8da8711eb47f7ac51bc897244491b03
Ustun Ertas <ustunertas0@gmail.com>
2023-03-16 22:05:42 +0300

    project

--- src/practice_basic_day03/Q16_DoWhileLoop02.java
+++ src/practice_basic_day03/Q16_DoWhileLoop02.java
@@ -1,5 +1,7 @@
 package practice_basic_day03;
 
+import java.util.Scanner;
+
 public class Q16_DoWhileLoop02 {
     /* WhileLoop,DoWhile
 		Ask user to enter a word. If the word has odd number of characters
@@ -11,5 +13,21 @@ public class Q16_DoWhileLoop02 {
 		     INPUT      :   elif
 		     OUTPUT 	:   You entered wrong word.*/
 
+    public static void main(String[] args) {
 
-}
+                Scanner scanner = new Scanner(System.in);
+                System.out.print("Bir kelime girin: ");
+                String kelime = scanner.nextLine();
+
+                int uzunluk = kelime.length();
+                int orta = uzunluk / 2;
+
+                if (uzunluk % 2 == 1 && uzunluk >= 3) {
+                    System.out.println(kelime.charAt(orta));
+                } else {
+                    System.out.println("Yanlış kelime girdiniz.");
+                }
+            }
+        }
+
+
